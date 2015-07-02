6cea7198dfa82bba10b15bd0a448ef64ab50167b
Louis Rose <louismrose@gmail.com>
2015-07-02 17:27:29 +0100

    Remove superfluous method.

--- tools/complexity/lib/measurement/condition_counter.rb
+++ tools/complexity/lib/measurement/condition_counter.rb
@@ -2,15 +2,5 @@ require_relative "counter"
 
 module Measurement
   class ConditionCounter < Counter
-
-    private
-
-    def leaves(node)
-      if node.respond_to?(:children) && !node.children.empty?
-        node.children.flat_map { |child| leaves(child) }
-      else
-        [node]
-      end
-    end
   end
 end
